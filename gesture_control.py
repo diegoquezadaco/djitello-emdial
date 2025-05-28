6f58b6e681dd67361f66dc3ee65f619709d49bea
Diego Quezada <a01710024@tec.mx>
2025-05-27 19:10:45 -0600

    gestures proposed

--- gesture_control.py
+++ gesture_control.py
@@ -3,12 +3,22 @@ import mediapipe as mp
 from djitellopy import Tello
 import time
 
-# Inicializar MediaPipe Face Detection
-mp_face_detection = mp.solutions.face_detection
-# Inicializar MediaPipe Hands
 mp_hands = mp.solutions.hands
-# Inicializar MediaPipe Pose
-mp_pose = mp.solutions.pose
+mp_drawing = mp.solutions.drawing_utils
+hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.7)
+
+# Coordenadas de los puntos de referencia de la mano
+wrist = 0
+thumb_mcp = 2
+thumb_tip = 4
+index_base = 5
+index_tip = 8
+middle_tip = 12
+middle_mcp = 19
+ring_tip = 16
+ring_mcp = 13
+pinky_tip = 20
+pinky_mcp = 17
 
 # Conectar al dron
 drone = Tello()
@@ -50,6 +60,13 @@ def control():
             continue
 
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # Flip para espejo
+        frame = cv2.flip(frame, 1)
+        h, w, _ = frame.shape
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        result = hands.process(rgb)
+        label = "No se detecta mano"
         cv2.putText(frame, "Battery: {}%".format(drone.get_battery()), 
                     (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
         cv2.putText(frame, "Height: {}cm".format(drone.get_height()), 
@@ -74,7 +91,50 @@ def control():
             
         
         cv2.imshow('Video Stream', frame)
-        key = cv2.waitKey(50) & 0xFF
+        if result.multi_hand_landmarks and result.multi_handedness:
+            hand_landmarks = result.multi_hand_landmarks[0]
+            handedness = result.multi_handedness[0]
+
+            landmarks = hand_landmarks.landmark
+            if landmarks[middle_tip].y < landmarks[middle_mcp].y and landmarks[ring_tip].y < landmarks[ring_mcp].y and landmarks[pinky_tip].y < landmarks[pinky_mcp].y and landmarks[thumb_tip].y < landmarks[thumb_mcp].y and landmarks[index_tip].y < landmarks[index_base].y:
+                label = "Takeoff"
+        
+                if flying:
+                    pass
+                else:
+                    if drone.get_battery() <= 15 and not flying:
+                        pass
+                    else:
+                        drone.takeoff()
+                        flying = True
+            elif landmarks[middle_tip].y > landmarks[middle_mcp].y and landmarks[ring_tip].y > landmarks[ring_mcp].y and landmarks[pinky_tip].y > landmarks[pinky_mcp].y and landmarks[index_tip].y > landmarks[index_base].y:
+                label = "Land"
+                cmd = 1
+                if flying:
+                    drone.land()
+                    flying = False
+                else:
+                    pass
+            elif landmarks[pinky_tip].y < landmarks[pinky_mcp].y and landmarks[thumb_tip].x > landmarks[thumb_mcp].x and landmarks[index_tip].y > landmarks[index_base].y and landmarks[middle_tip].y > landmarks[middle_mcp].y and landmarks[ring_tip].y > landmarks[ring_mcp].y:
+                label = "Up"
+                cmd = 2
+            elif landmarks[pinky_tip].y > landmarks[pinky_mcp].y and landmarks[thumb_tip].y > landmarks[thumb_mcp].y and landmarks[index_tip].y > landmarks[wrist].y and landmarks[middle_tip].y > landmarks[wrist].y and landmarks[ring_tip].y > landmarks[wrist].y:
+                label = "Down"
+                cmd = 3
+            elif landmarks[thumb_tip].y < landmarks[thumb_mcp].y and landmarks[index_tip].y < landmarks[index_base].y and landmarks[thumb_tip].x < landmarks[pinky_mcp].x and landmarks[middle_tip].y > landmarks[middle_mcp].y and landmarks[ring_tip].y > landmarks[ring_mcp].y and landmarks[pinky_tip].y > landmarks[pinky_mcp].y:
+                label = "Right"
+                cmd = 4
+            elif landmarks[thumb_tip].y < landmarks[thumb_mcp].y and landmarks[index_tip].y < landmarks[index_base].y and landmarks[thumb_tip].x > landmarks[pinky_mcp].x and landmarks[middle_tip].y > landmarks[middle_mcp].y and landmarks[ring_tip].y > landmarks[ring_mcp].y and landmarks[pinky_tip].y > landmarks[pinky_mcp].y:
+                label = "Left"
+                cmd = 5
+            elif landmarks[thumb_tip].y < landmarks[thumb_mcp].y and landmarks[index_tip].y < landmarks[index_base].y and landmarks[thumb_tip].x < landmarks[pinky_mcp].x and landmarks[middle_tip].y > landmarks[middle_mcp].y and landmarks[ring_tip].y > landmarks[ring_mcp].y and landmarks[pinky_tip].y < landmarks[pinky_mcp].y:
+                label = "Front"
+                cmd = 6
+            elif landmarks[thumb_tip].y < landmarks[thumb_mcp].y and landmarks[index_tip].y < landmarks[index_base].y and landmarks[thumb_tip].x > landmarks[pinky_mcp].x and landmarks[middle_tip].y > landmarks[middle_mcp].y and landmarks[ring_tip].y > landmarks[ring_mcp].y and landmarks[pinky_tip].y < landmarks[pinky_mcp].y:
+                label = "Back"
+                cmd = 7
+            print(f"Detected command: {label}")
+
         
 
         '''if drone.get_height() > 300 and ud_vel > 0:
